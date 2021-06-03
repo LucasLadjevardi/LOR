a779b2ad1faeb083593c6209db5766a36cbd2c88
rasmusrbrune <rasmus14.rosenqvist@gmail.com>
2021-06-03 08:53:02 +0200

    import comp

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { StoreComponent } from './store/store.component';
 
 const routes: Routes = [
   {
