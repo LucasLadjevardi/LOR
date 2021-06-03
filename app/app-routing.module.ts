a0ef8fe4b97bf1dcb2ec2510a75e26c9a80fe821
rasmusrbrune <rasmus14.rosenqvist@gmail.com>
2021-06-03 08:17:15 +0200

    Rasmus 3/6  home page/routing

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'store',
+    component: StoreComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -11,5 +11,15 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  DownloadLauncher(){
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../favicon.ico');
+    link.setAttribute('download', `TestDownload.ico`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
 
 }
