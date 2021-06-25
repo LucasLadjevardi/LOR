69ab417c2d72f6aa937b2c37937a8554ca078506
rasmusrbrune <rasmus14.rosenqvist@gmail.com>
2021-06-25 10:31:13 +0200

    25/06/2021 Login navbar changed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output, ɵɵNgOnChangesFeature } from '@angular/core';
 import {Router} from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { BehaviorSubject, Observable } from 'rxjs';
 import * as $ from 'jquery';
 import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 
@@ -16,7 +17,13 @@ export class LoginComponent implements OnInit {
   public RepeatPassword="";
   public Email="";
  
-  
+  /*det er en sub-type af en Observable med en BehaviorSubject skal objecte som man får BehaviorSubject på have en value,
+  da det hele tiden sender value til sige "subscription" hvor en observable kun vil sendt det når onnext bliver køret*/ 
+  public ProfileBehavior: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false); 
+  //en ting som man kan gjorde med en BehaviorSubject få en observable som vi har gjordt her
+  public IsLogged: Observable<boolean> = this.ProfileBehavior.asObservable();
+  //en Observable er en function som holder øjer med value/Subject og med den kan man sig at hvis der sker en noget så gøre det her fx en alertbox 
+
   constructor(private _router: Router, private _NavBar :NavbarComponent) {
 
   }
@@ -49,10 +56,10 @@ export class LoginComponent implements OnInit {
   AccountLogIn(){
     if(this.UserName!=" " && this.Password!=" "){
       this._router.navigate(['/home']);
-      this._NavBar.TestToSeeIfUserIsLogin(true)
+      //this._NavBar.TestToSeeIfUserIsLogin(true)
     }
     else{
-      this._NavBar.TestToSeeIfUserIsLogin(false)
+      //this._NavBar.TestToSeeIfUserIsLogin(false)
     }
     
   }
@@ -61,5 +68,9 @@ export class LoginComponent implements OnInit {
     if(this.RepeatPassword==this.Password){
       console.log("Account Create welcome", this.UserName)
     }
+  }
+
+  LoginHelp(){
+
   }
 }
