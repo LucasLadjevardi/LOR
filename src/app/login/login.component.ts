cc8d8e4f0a21a26634d564b2c3d2b8f5bb958f9f
GraViiX <lucladjevardi@gmail.com>
2021-06-29 09:37:22 +0200

    Changes to login callbacks

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,30 +29,55 @@ export class LoginComponent implements OnInit {
   }
   
   
-
+  // http://192.168.4.110:48935/api/Users/username/password
   AccountLogin(){
-      var UserCheck: string | any[];
+      var UserCheck: any;
+      var url = `http://192.168.4.110:48935/api/Users/${this.UserName}/${this.Password}`
       var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "http://192.168.4.110:48935/api/Users",
-        "method": "GET"
+        "url": url,
+        "method": "GET",
+        "error": function (jqXHR: { status: number; }, exception: any){
+          if(jqXHR.status == 404){
+            alert("Username and/or password was incorrect")
+          }
+        }
       }
       $.ajax(settings).done( (response) => {
         UserCheck = response;
       })
       .then( () => {
-        for (let i = 0; i < UserCheck.length; i++) {
-          var Users = UserCheck[i];
-          if(this.UserName == Users.username && this.Password == Users.password)
-          {
-            this._LoginService.ProfileBehavior.next(true);
-            this._router.navigate(['/home']);
-          }
+        if(UserCheck.username == this.UserName && UserCheck.password == this.Password)
+        {
+          this._LoginService.ProfileBehavior.next(true);
+          this._router.navigate(['/home']);
         }
       });
   }
   
+  /*AccountLogin(){
+    var UserCheck: string | any[];
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://192.168.4.110:48935/api/Users",
+      "method": "GET"
+    }
+    $.ajax(settings).done( (response) => {
+      UserCheck = response;
+    })
+    .then( () => {
+      for (let i = 0; i < UserCheck.length; i++) {
+        var Users = UserCheck[i];
+        if(this.UserName == Users.username && this.Password == Users.password)
+        {
+          this._LoginService.ProfileBehavior.next(true);
+          this._router.navigate(['/home']);
+        }
+      }
+    });
+}*/
 
   CreateAccount(){
     if(this.RepeatPassword==this.Password){
