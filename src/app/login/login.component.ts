bcce73228d857dc3f1015a6a08f17bbde4e64d0b
GraViiX <lucladjevardi@gmail.com>
2021-07-01 14:45:24 +0200

    login changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import * as $ from 'jquery';
 import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +14,15 @@ import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 })
 export class LoginComponent implements OnInit {
 
-  public UserName = "";
-  public Password = "";
-  public RepeatPassword = "";
-  public Email = "";
- 
+  public UserData = [{
+    id: "",
+    Username: "",
+    Password: "",
+    RepeatPassword: "",
+    Email: "",
+    Role: "User"
+  }];
   
-
   constructor(private _router: Router, private _LoginService :LoginService) {
 
   }
@@ -32,7 +35,7 @@ export class LoginComponent implements OnInit {
   // http://192.168.4.110:48935/api/Users/username/password
   AccountLogin(){
       var UserCheck: any;
-      var url = `http://192.168.4.110:48935/api/Users/${this.UserName}/${this.Password}`
+      var url = `http://192.168.4.110:48935/api/Users/${this.UserData[1]}/${this.UserData[2]}`
       var settings = {
         "async": true,
         "crossDomain": true,
@@ -42,13 +45,17 @@ export class LoginComponent implements OnInit {
           if(jqXHR.status == 404){
             alert("Username and/or password was incorrect")
           }
+          else if(jqXHR.status == 400)
+          {
+            alert("Username and/or password was incorrect")
+          }
         }
       }
       $.ajax(settings).done( (response) => {
         UserCheck = response;
       })
       .then( () => {
-        if(UserCheck.username == this.UserName && UserCheck.password == this.Password)
+        if(UserCheck.username == this.UserData[1] && UserCheck.password == this.UserData[2])
         {
           this._LoginService.ProfileBehavior.next(true);
           this._router.navigate(['/home']);
@@ -79,9 +86,24 @@ export class LoginComponent implements OnInit {
     });
 }*/
 
-  CreateAccount(){
-    if(this.RepeatPassword==this.Password){
-      console.log("Account Create welcome", this.UserName)
+  CreateAccount(UserData: any){
+    if(UserData[2] == UserData[3]){
+      $.ajax({
+        url:"http://192.168.4.110:48935/api/Users",
+        type:"POST",
+        async:true,
+        crossDomain:true,
+        dataType:"json",
+        data:JSON.stringify({
+         "username": UserData[1],
+         "password": UserData[2],
+         "email": UserData[4],
+         "role": "User"
+        }),
+        contentType:"application/json; charset=utf-8",
+    }); 
+    } else if(UserData[2] != UserData[3]){
+      
     }
   }
 
